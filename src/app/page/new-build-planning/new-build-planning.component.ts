4ee4b2f57e8ddb547ec4e585b64ea87e5dfee8ef
XYXA05 <bogdansavi05@gmail.com>
2024-08-06 07:59:04 +0200

    ewrgew

--- src/app/page/new-build-planning/new-build-planning.component.ts
+++ src/app/page/new-build-planning/new-build-planning.component.ts
@@ -71,6 +71,7 @@ export class NewBuildPlanningComponent implements OnInit{
       this.jsonDataDescriptions = [...this.jsonDataDescriptionsOriginal];
       this.uniqueTypeItems = [...new Set<string>(data.map((item: any) => item.type_items))];
       this.uniqueFeaturesItems = [...new Set<string>(data.map((item: any) => item.Features))];
+      this.cdr.detectChanges(); // Trigger change detection
     });
   }
   public getMethodOwner(ownerId: number){
